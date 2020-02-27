0be85f3d7189371fb7f7e6ed9210fa3f7b84f014
Jorge Casariego <jorgekasa@gmail.com>
2020-02-27 14:38:04 -0300

    Day 16: Creating some kind of Forms using Sections

---
+++ Day16/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Jorge Casariego on 2/27/20.
+//  Copyright © 2020 Jorge Casariego. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Form {
+            Section {
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                
+            }
+            
+            Section {
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
+        
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
