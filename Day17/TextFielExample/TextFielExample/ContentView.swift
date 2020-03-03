5cf51089ff71a598f039c98b9003fd6ad11da4e5
Jorge Casariego <jorgekasa@gmail.com>
2020-03-02 23:57:21 -0300

    Day17: Reading text from the user with TextField

---
+++ Day17/TextFielExample/TextFielExample/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  TextFielExample
+//
+//  Created by Jorge Casariego on 3/2/20.
+//  Copyright © 2020 Jorge Casariego. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section {
+                Text("$\(checkAmount)")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
